d8031d8c93c74e410e4cec22ba14efe6a058849b
dinari-roberto-carlos <roberto.carlos@dinaribank.com.br>
2021-04-15 14:16:15 -0300

    first commit

---
+++ vue_front/src/components/layouts/NavBarVue.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <nav v-bind:class="cor || 'blue'">
+      <div class="nav-wrapper container">
+        <a v-bind:href="url || '#'" class="brand-logo center">{{logo || 'Site'}}</a>
+        <ul id="nav-mobile" class="left hide-on-med-and-down">
+          <li><a href="sass.html">Sass</a></li>
+          <li><a href="badges.html">Components</a></li>
+          <li><a href="collapsible.html">JavaScript</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+<script>
+export default {
+  name: 'NavBarVue',
+  props: ['logo', 'url', 'cor']
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue_front/src/pages/home/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <span>
+    <div class="row">
+      <grid-vue class="input-field" tamanho="12">
+        <textarea id="textarea1" class="materialize-textarea"></textarea>
+        <label for="textarea1">O que está acontecendo?</label>
+      </grid-vue>
+      <p>
+        <grid-vue tamanho="2 offset-s10" class="waves-effect waves-light btn">Publicar</grid-vue><!-- Botao -->
+      </p>
+    </div>
+    <card-conteudo-vue perfil="http://materializecss.com/images/yuna.jpg" nome="Rodrigo Teste" data="17/02/2021 - 16:55">
+      <card-detalhe-vue img="https://materializecss.com/images/sample-1.jpg" titulo="Imagem Nova" txt="Valores aleatorios">
+      </card-detalhe-vue>
+    </card-conteudo-vue>
+  </span>
+</template>
+
+<script>
+import CardConteudoVue  from '@/components/social/CardConteudoVue'
+import CardDetalheVue from '@/components/social/CardDetalheVue'
+import GridVue from '@/components/layouts/GridVue'
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+    }
+  },
+  components: {
+    CardConteudoVue,
+    CardDetalheVue,
+    GridVue
+  }
+}
+</script>
+<!-- Add " CardConteudoVuescoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
