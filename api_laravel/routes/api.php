d658f74f242fe77568f05f24d3e6a7ba46236b40
Roberto <betobr12@users.noreply.github.com>
2021-06-22 11:02:20 -0300

    Update api.php

--- api_laravel/routes/api.php
+++ api_laravel/routes/api.php
@@ -8,41 +8,6 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-/*Route::get('testes', function() {
-
-    $user = App\Models\User::find(1);
-    $user2 = App\Models\User::find(3);//para adicionar aos amigos
-
-
-
-    //adicionar curtidas
-   /*
-    */
-//adicionar comentarios
-/*
-    $content = App\Models\Content::find(1);
-    $user->comments()->create([
-        'content_id'   =>$content->id,
-        'description'  =>'Teste de comentario',
-        'created_at'   =>\Carbon\Carbon::now() //date('Y-m-d')
-    ]);
-
-
-
-    });
-*/
-
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login',    [UserController::class, 'login']);
 
