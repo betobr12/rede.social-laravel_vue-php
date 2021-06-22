52ec43cef825e62bdbfbb5817fb8b8b1122eb31a
Roberto <betobr12@users.noreply.github.com>
2021-06-22 11:22:24 -0300

    Update Post.vue

--- vue_front/src/pages/post/Post.vue
+++ vue_front/src/pages/post/Post.vue
@@ -1,51 +1,7 @@
 <template>
   <post-template>
     <span slot="principal">
-      <!--
-      <div class="row">
-        <div class="card">
-          <div class="card-content">
-            <div class="row valign-wrapper">
-              <grid-vue tamanho="1">
-                 <img class="circle responsive-img">
-              </grid-vue>
-              <grid-vue tamanho="11">
-                <span class="black-text">
-                   <strong>Nome</strong> - <small>Data</small>
-                </span>
-              </grid-vue>
-            </div>
-            <slot />
-          </div>
-          <div class="card-action">
-            <p>
-              <a style="cursor:pointer">
-                <i class="material-icons"></i> 10
-              </a>
-              <a style="cursor:pointer" >
-                <i class="material-icons">insert_comment</i>
-              </a>
-            </p>
-            <p  class="right-align">
-              <input type="text"  placeholder="Comentar">
-              <button  class="btn waves-effect waves-light orange"><i class="material-icons">send</i></button>
-            </p>
-            <p >
-              <ul class="collection">
-                <li class="collection-item avatar" >
-                  <img  class="circle">
-                  <span class="titles">Username <small>- Criado em</small></span>
-                  <p>
-                   Descrição
-                  </p>
-                </li>
-              </ul>
-            </p>
-          </div>
-        </div>
-      </div>
--->
-
+      <!-- Projeto Visualizar Post-->
     </span>
   </post-template>
 </template>
