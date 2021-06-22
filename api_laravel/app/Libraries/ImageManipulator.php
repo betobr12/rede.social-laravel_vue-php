71052f4d2061eae6e9b3317df458d556c35a9b8b
Roberto <betobr12@users.noreply.github.com>
2021-06-22 11:48:00 -0300

    Update ImageManipulator.php

--- api_laravel/app/Libraries/ImageManipulator.php
+++ api_laravel/app/Libraries/ImageManipulator.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Storage;
 class ImageManipulator
 {
     public $image, $id, $image_name, $directory, $server = 'http://127.0.0.1:8000/';
-    //modificar server na produção
+    
     public function pathImageCreate()
     {
         $ext_full = substr($this->image, 11, strpos($this->image, ';') - 11);
@@ -51,38 +51,5 @@ class ImageManipulator
         } else {
             return $imageName = $this->image_name;
         }
-    }
-    /*
-    public function pathImageCreateOld(){
-
-        if (isset($this->image)) {
-            $time = time();
-           // $diretory_imagem = $this->directory.DIRECTORY_SEPARATOR.$this->id_directory.$this->id;
-            $diretory_imagem = $this->directory.'/'.$this->id_directory.$this->id;
-            $ext = substr($this->image, 11, strpos($this->image, ';') - 11);
-           // $url_image = $diretory_imagem.DIRECTORY_SEPARATOR.$time.'.'.$ext;
-            $url_image = $diretory_imagem.'/'.$time.'.'.$ext;
-            $file = str_replace('data:image/'.$ext.';base64,','',$this->image);
-            $file = base64_decode($file);
-
-            if (!file_exists($this->directory)) {
-              mkdir($this->directory,0700);
-            }
-
-            if($this->image_data_exists){
-                if(file_exists($this->image_data_exists)){
-                  unlink($this->image_data_exists);
-                }
-            }
-
-            if (!file_exists($diretory_imagem)) {
-              mkdir($diretory_imagem,0700);
-            }
-            file_put_contents($url_image,$file);
-            $user_image = $this->server.$url_image;
-
-            return $user_image;
-        }
-    }
-    */
+    }    
 }
